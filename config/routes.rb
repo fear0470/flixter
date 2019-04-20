89d3a2a7d6d7e5f6b37ec256a8d54b5ef5065689
Nicholas Fearns <you@example.com>
2019-04-20 22:31:05 +0000

    created a lessons controller to students and began to style the student lessons page in advance of additional features

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
+  resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create] 
