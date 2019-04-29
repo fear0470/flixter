bb4c37ba8df151dd4416e781d7ed5830ac954a2c
Nicholas Fearns <you@example.com>
2019-04-29 01:24:00 +0000

    installed imagemagick and minimagick to automatically resize images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -22,7 +22,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process resize_to_fill: [800, 350]
+
   #
   # def scale(width, height)
   #   # do something
