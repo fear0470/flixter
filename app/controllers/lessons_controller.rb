95587d1dbea85f680468bdbc67897987e68329e7
Nicholas Fearns <you@example.com>
2019-04-30 21:59:10 +0000

    added permission requirements for enrollment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,10 @@
 class LessonsController < ApplicationController
 
+  before_action :authenticate_user!
+    before_action :require_authorized_for_current_lesson, :only => [:show]
+
+
+
    def show
    end
 
@@ -9,5 +14,12 @@ class LessonsController < ApplicationController
    def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+
+  def require_authorized_for_current_lesson
+    if ! current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => "You must be enrolled!"
+    end
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,13 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
+  resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
-      resources :lessons, only: [:new, :create] 
+      resources :lessons, only: [:new, :create]
     end
-
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
-     
     end
   end
 end
