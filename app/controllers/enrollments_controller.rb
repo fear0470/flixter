50f7e5923060d83f6159d5962183f3c87b9211f4
Nicholas Fearns <you@example.com>
2019-05-03 22:24:00 +0000

    fixing enrollment controller

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,9 +2,9 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-     if current_course.premium?
+    if current_course.premium?
 
-  # Amount in cents
+    # Amount in cents
     @amount = (current_course.cost * 100).to_i
 
     customer = Stripe::Customer.create(
@@ -15,22 +15,22 @@ class EnrollmentsController < ApplicationController
     charge = Stripe::Charge.create(
       customer: customer.id,
       amount: @amount,
-      description: 'Flixter Primo Content',
-      currency: 'usd'
+      description: 'Flixter Premo Content',
+      currency: 'CAD'
     )
   end
 
-  current_user.enrollments.create(course: current_course)  
+  current_user.enrollments.create(course: current_course)
   redirect_to course_path(current_course)
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to root_path
+  end
 
-private
+  private
 
-def current_course
-  @current_course ||= Course.find(params[:course_id])
-end
-
+  def current_course
+    @current_course ||=Course.find(params[:course_id])
+  end
 end
