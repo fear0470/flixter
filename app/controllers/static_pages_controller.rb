e44e61a49c090db245e93f1f32d3f30a9b5c96b9
Nicholas Fearns <you@example.com>
2019-05-06 22:01:55 +0000

    added links for static pages in footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,14 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  def privacy
+  end
+
+  def careers
+  end
+
+  def team
+  end
+
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
-
-  
+  get 'privacy', to: 'static_pages#privacy'
+  get 'careers', to: 'static_pages#careers'
+  get 'team', to: 'static_pages#team'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
